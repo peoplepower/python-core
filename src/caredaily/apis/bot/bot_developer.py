2304c5ace29fc20efb1dea137c98dc5c048333f7
Destry Teeter <destry.teeter@gmail.com>
2026-03-27 18:14:41 -0700

    Fixed botlab_store should allow admin key

--- src/caredaily/apis/bot/bot_developer.py
+++ src/caredaily/apis/bot/bot_developer.py
@@ -223,8 +223,6 @@ class BotDeveloper(API):
         """
         params = {"bundle": bundle}
         params = {k: v for k, v in params.items() if v is not None}
-        headers = self.adapter._get_headers()
-        print("Headers:", headers)
         return self.adapter.get("/espapi/cloud/developer/apps", ep_params=params)
 
     def get_bot_versions(
